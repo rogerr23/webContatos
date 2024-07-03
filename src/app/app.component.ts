efb043697532adeac7f9d117058b66a6e92da70b
rogerr23 <roger17ribeiro09@gmail.com>
2024-07-03 18:41:04 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { subscribe } from 'diagnostics_channel';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  contatos: any[] = [];
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+
+    this.httpClient.get("http://localhost:8082/api/contatos")
+      .subscribe({
+        next: (data) => {
+          this.contatos = data as any[];
+        },
+        error: (e) => {
+          console.log(e.error);
+        }
+      })
+  }
+
+
+
+
   title = 'webContatos';
 }
